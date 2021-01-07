5a016f6c9735960a3a1646ec48d1acdc82304bba
rochads <72424771+rochads@users.noreply.github.com>
2021-01-07 00:56:52 -0300

    Aula 34 - Formulário e estilizações
    Iniciou construção de formulário de adição de categorias.

--- routes/admin.js
+++ routes/admin.js
@@ -9,8 +9,16 @@ admin.get('/posts', (req, res) => {
     res.send("Página de posts!")
 })
 
-admin.get('/categorias', (req, res) => {
-    res.send("Página de categorias!")
+admin.get('/categories', (req, res) => {
+    res.render('admin/categories')
+})
+
+admin.get('/categories/add', (req, res) => {
+    res.render('admin/addcategories')
+})
+
+admin.post('/categories/new', (req, res) => {
+    res.send('Isso não estava na aula 34!!!')
 })
 
 module.exports = admin
